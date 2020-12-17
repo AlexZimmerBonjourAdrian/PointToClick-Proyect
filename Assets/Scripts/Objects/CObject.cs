7531467e6a374e9f1eb7484dde0e19448089c247
AexZimmer <alxzimmer.zimmer1@gmail.com>
2020-12-16 21:55:42 -0300

    init Point to click

---
+++ Assets/Scripts/Objects/CObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CObject : MonoBehaviour, Iinteract
+{
+   public void Oninteract()
+    {
+        Debug.Log("Interactua");
+    }
+}
