587026aee2d79124229fcb2f09e79512e964fdb8
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2025-03-06 21:42:34 -0300

    Order and Sort new Files

--- Assets/PointToClick-Engine/Script/Controller/CSaveSystem.cs
+++ Assets/PointToClick-Engine/Script/Controller/CSaveSystem.cs
@@ -3,33 +3,33 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 public static class CSaveSystem 
 {
-    public static void SavePlayer(CPlayer player)
-    {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/player.fun";
-        FileStream stream = new FileStream(path, FileMode.Create);
+    // public static void SavePlayer(CPlayer player)
+    // {
+    //     BinaryFormatter formatter = new BinaryFormatter();
+    //     string path = Application.persistentDataPath + "/player.fun";
+    //     FileStream stream = new FileStream(path, FileMode.Create);
 
-        CPlayerData data = new CPlayerData(player);
+    //     CPlayerData data = new CPlayerData(player);
 
-        formatter.Serialize(stream, data);
-        stream.Close();
-    }
-    public static CPlayerData LoadPlayer ()
-    {
-        string path = Application.persistentDataPath + "/player.fun";
-        if(File.Exists(path))
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(path, FileMode.Open);
+    //     formatter.Serialize(stream, data);
+    //     stream.Close();
+    // }
+    // public static CPlayerData LoadPlayer ()
+    // {
+    //     string path = Application.persistentDataPath + "/player.fun";
+    //     if(File.Exists(path))
+    //     {
+    //         BinaryFormatter formatter = new BinaryFormatter();
+    //         FileStream stream = new FileStream(path, FileMode.Open);
 
-            CPlayerData data = formatter.Deserialize(stream) as CPlayerData;
-            stream.Close();
-            return data;
-        }
-        else
-        {
-            Debug.Log("Save file not found in " + path);
-            return null;
-        }
-    }
+    //         CPlayerData data = formatter.Deserialize(stream) as CPlayerData;
+    //         stream.Close();
+    //         return data;
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Save file not found in " + path);
+    //         return null;
+    //     }
+    // }
 }
--- Assets/PointToClick-Engine/Script/Data/scriptableObject/SaveAndLoadSystem/CPlayerData.cs
+++ Assets/PointToClick-Engine/Script/Data/scriptableObject/SaveAndLoadSystem/CPlayerData.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class CPlayerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public int level;
-    public int health;
-    public float[] position;
+    // // Start is called before the first frame update
+    // public int level;
+    // public int health;
+    // public float[] position;
 
-    public CPlayerData(CPlayer player)
-    {
-        level = player.level;
-        health = player.health;
+    // public CPlayerData(CPlayer player)
+    // {
+    //     level = player.level;
+    //     health = player.health;
 
-        position = new float[3];
+    //     position = new float[3];
 
-        position[0] = player.transform.position.x;
-        position[1] = player.transform.position.y;
-        position[2] = player.transform.position.z;
+    //     position[0] = player.transform.position.x;
+    //     position[1] = player.transform.position.y;
+    //     position[2] = player.transform.position.z;
 
 
-    }
+    // }
 } 
